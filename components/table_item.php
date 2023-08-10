37fa8b594110b512fdb9d5f1bb67eff101240e40
ciaran <ciaranjotter@gmail.com>
2023-08-10 10:42:46 +0200

    added link to source data in table item which is displayed on home page

--- components/table_item.php
+++ components/table_item.php
@@ -42,6 +42,8 @@
      <table>
      <tr><td class='table-label'>Description</td><td><?php echo $t->get_description() ?></td></tr>
 
+     <tr><td class='table-label'>Source file</td><td><a href='<?php echo $t->source;?>' target="_blank">Link</a></td></tr>
+
      <tr><td class='table-label'>Last Modified</td><td><?php echo date('D, j M Y',strtotime($t->get_update())) ?></td></tr>
     <?php
      if (count($t->get_tags())) {
