6af1b068dc2bb6051ae01e95e989e0c77da26822
Ciaran Otter <ciaranotter@Ciarans-MacBook-Air.local>
2024-06-19 12:23:05 +0200

    updated with universal save function with error and results printing

--- api/update.php
+++ api/update.php
@@ -5,57 +5,53 @@ $post = json_decode(file_get_contents("php:://input"), true);
 include_once('../init.php');
 include_once("../classes/query_handler.php");
 
-if ($id_list = query_handler::fetch_table_ids()) {
+$GLOBALS['directory'] = "../local_data/";
+$GLOBALS['archive_dir'] = "../local_data/archive/";
+
+
+function save_file($data,$file_name, $ext) {
+    $fp = fopen($GLOBALS['directory']. $file_name . $ext, "x");
+
+    if ($fp === false) {
+        $zip = new ZipArchive();
+        $zip->open($GLOBALS['archive_dir'].$file_name."_archive.zip", ZipArchive::CREATE);
+        $zip->addFile($GLOBALS['directory'] . $file_name . $ext, str_replace(" ", "_", date('Y-m-d_H_i_s')."_".$file_name.$ext));
+        $zip->close();
+
+        $fp = fopen($GLOBALS['directory']. $file_name . $ext, "w");
+    }
+
+    fwrite($fp, $data);
+    fclose($fp);
+}
+
+
+if (isset($_GET['table'])) {
+    $table = query_handler::fetch_table_by_id($_GET['table']);
+
+    if ($table->set_data($table->get_source()) == false) {
+        die(json_encode(array("error" => $table->error)));
+    }
+
+    save_file($table->get_csv_string(), $table->get_id(), ".csv");
+    save_file(json_encode($table->generate_json()), $table->get_id(), ".json");
+    
+} else {
+    if ($id_list = query_handler::fetch_table_ids()) {
 
     $results = array();
     // foreach ($id_list as $id){
-        $table = query_handler::fetch_table_by_id($id_list[0]);
-        // $results[$id] = array();
-        
-        if ($table->set_data($table->get_source()) == false) {
-            json_encode(array("error" => $table->error));
-        }
+    $table = query_handler::fetch_table_by_id($id_list[0]);
+    // $results[$id] = array();
+    
+    if ($table->set_data($table->get_source()) == false) {
+        die(json_encode(array("error" => $table->error)));
+    }
 
-        // $csv_array = $table->get_csv_string();
-        // echo $csv_array;
+    save_file($table->get_csv_string(), $table->get_id(), ".csv");
+    save_file(json_encode($table->generate_json()), $table->get_id(), ".json");
 
-        $directory = "../local_data/";
-        $file_name = str_replace(" ", "_", $table->get_id()) . ".csv";
-
-        $fp = fopen($directory . $file_name, "x");
-        
-        if ($fp === false) {
-
-            $zip = new ZipArchive();
-            $zip->open("../local_data/archive/".$table->get_id()."_archive.zip", ZipArchive::CREATE);
-            $zip->addFile($directory . $file_name, str_replace(" ", "_", date('Y-m-d_H_i_s')."_".$file_name));
-            $zip->close();
-
-            $fp = fopen($directory . $file_name, "w");
-        }
-
-        fwrite($fp, $table->get_csv_string());
-        fclose($fp);
-
-        $file_name = $table->get_id(). ".json";
-
-        $fp = fopen($directory . $file_name, "x");
-
-        if ($fp === false) {
-            $zip = new ZipArchive();
-            $zip->open("../local_data/archive/".$table->get_id()."_archive.zip", ZipArchive::CREATE);
-            $zip->addFile($directory . $file_name, str_replace(" ", "_", date('Y-m-d_H_i_s')."_".$file_name));
-            $zip->close();
-
-            $fp = fopen($directory . $file_name, "w");
-        }
-
-        fwrite($fp, json_encode($table->generate_json()));
-        fclose($fp);
-        // fopen("")
-        // die("welp");
-    // }
-
+    }
 }
 
 ?>
