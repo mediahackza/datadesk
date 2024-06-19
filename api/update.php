3aaa2adc493ab805a661f77d521ab7fa7dd15205
Ciaran Otter <ciaranotter@Ciarans-MacBook-Air.local>
2024-06-19 12:48:37 +0200

    update to results system

--- api/update.php
+++ api/update.php
@@ -8,6 +8,7 @@ include_once("../classes/query_handler.php");
 $GLOBALS['directory'] = "../local_data/";
 $GLOBALS['archive_dir'] = "../local_data/archive/";
 
+$results = array();
 
 function save_file($data,$file_name, $ext) {
     $fp = fopen($GLOBALS['directory']. $file_name . $ext, "x");
@@ -28,30 +29,36 @@ function save_file($data,$file_name, $ext) {
 
 if (isset($_GET['table'])) {
     $table = query_handler::fetch_table_by_id($_GET['table']);
+    $results[$_GET['table']] = array();
 
     if ($table->set_data($table->get_source()) == false) {
-        die(json_encode(array("error" => $table->error)));
+        $results[$_GET['table']]["error"]  = $table->error;
     }
 
     save_file($table->get_csv_string(), $table->get_id(), ".csv");
+    $results[$_GET['table']]["csv"] = "successfully updated " . $table->get_name() . " csv local file.";
     save_file(json_encode($table->generate_json()), $table->get_id(), ".json");
-    
+    $results[$_GET['table']]['json'] = "successfully updated " . $table->get_name() . " json local file.";
+
 } else {
     if ($id_list = query_handler::fetch_table_ids()) {
 
-    $results = array();
-    // foreach ($id_list as $id){
-    $table = query_handler::fetch_table_by_id($id_list[0]);
-    // $results[$id] = array();
-    
-    if ($table->set_data($table->get_source()) == false) {
-        die(json_encode(array("error" => $table->error)));
-    }
+        foreach ($id_list as $id){
+            $table = query_handler::fetch_table_by_id($id);
+            $results[$id] = array();
+            
+            if ($table->set_data($table->get_source()) == false) {
+                $results[$id]["error"]  = $table->error;
+            }
 
-    save_file($table->get_csv_string(), $table->get_id(), ".csv");
-    save_file(json_encode($table->generate_json()), $table->get_id(), ".json");
-
+            save_file($table->get_csv_string(), $table->get_id(), ".csv");
+            $results[$id]["csv"] = "successfully updated " . $table->get_name() . " csv local file.";
+            save_file(json_encode($table->generate_json()), $table->get_id(), ".json");
+            $results[$id]['json'] = "successfully updated " . $table->get_name() . " json local file.";
+        }
     }
 }
+
+echo json_encode($results);
 
 ?>
