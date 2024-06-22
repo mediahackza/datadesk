096e46aca47d60b6a1240c927dcadbe0aeccf66c
Alastair Otter <alastair@mediahack.co.za>
2024-06-22 17:54:06 +0200

    Adding error checking

--- api/update.php
+++ api/update.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // $post = json_decode(file_get_contents("php:://input"), true);
 
 include_once('../init.php');
